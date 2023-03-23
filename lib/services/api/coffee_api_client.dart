e08b74f3ffc2c31ae1fa666975cd39bf24f66dcf
mihiro <f.mihiroxx@gmail.com>
2023-03-23 15:38:08 +0900

    リファクタリング

--- lib/services/api/coffee_api_client.dart
+++ lib/services/api/coffee_api_client.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_package_sample/models/api/request/coffee.dart';
 
@@ -5,6 +7,9 @@ class CoffeeApiClient {
   Future<List<Coffee>?> fetchList() async {
     final dio = Dio();
     const url = 'https://api.sampleapis.com/coffee/hot';
+
+    HttpOverrides.global = PermitInvalidCertification();
+
     final response = await dio.get(url);
 
     if (response.statusCode == 200) {
@@ -17,5 +22,14 @@ class CoffeeApiClient {
       }
     }
     return null;
+  }
+}
+
+// エラーの対策　https://devlights.hatenablog.com/entry/2021/08/27/145704
+class PermitInvalidCertification extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (cert, host, port) => true;
   }
 }
